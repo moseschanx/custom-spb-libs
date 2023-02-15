8dd1fbab1667d8acbe08482a37627d0175f3eeb7
moses-Desktop <moseschanx@gmail.com>
2023-02-15 22:33:22 +0800

    added footprint folder clean batch

---
+++ footprints/foorprint_folder_clean.bat
@@ -0,0 +1,33 @@
+
+del *.DRC
+::del *.opj
+del *.png
+del *.DBK
+del *.did
+del *.OBK
+del *~
+del *.log 
+del *.dml
+del *lck
+del *.jrl
+del *.tag
+del *txt*
+del *art*
+del *lst
+del *sav
+del *xml
+del *.TMP
+del *.do
+del *.orig
+del *rpt*
+del *dmp*
+del *.rtcomp
+del *.sts
+del *.drl
+
+
+del /F /Q signoise.run
+rmdir /S /Q signoise.run
+rmdir /S /Q stepFacetFiles4Map
+
+
